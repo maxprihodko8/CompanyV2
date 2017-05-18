1c84116d99e9b007c0c6d17eae8b3a5ca1037b31
pn-user30 <pm@digithive.ru>
2017-05-18 17:24:40 +0300

    Added some info to migration

--- console/migrations/m170518_132104_tender.php
+++ console/migrations/m170518_132104_tender.php
@@ -75,6 +75,37 @@ class m170518_132104_tender extends Migration
             'CASCADE'
         );
 
+        if(\common\models\service\UserService::getUserByUserName('admin') === null) {
+            $this->insert('user', [
+                'id' => 1,
+                'username' => 'admin',
+                'password_hash' => '$2a$04$pDhBD41yVVZJN4bxdYt3ceDinoPKNhvtQeycQjIBVUercYhf9JOoS', //// 'admin'
+                'status' => '10',
+                'auth_key' => 'auth-admin_new231321332',
+                'email' => 'admin@admin.com',
+                'created_at' => time(),
+                'updated_at' => time(),
+            ]);
+        }
+
+        $this->batchInsert('company', ['id', 'name', 'adress', 'contact_user', 'phone', 'register_date'], [
+            [1, 'Company1', 'Sumy', 'SomeUser1', '+382344224', '2017-05-16 17:16:59'],
+            [2, 'Company2', 'Kiev', 'SomeUser2', '+324244322', '2017-05-17 12:13:59'],
+            [3, 'Company3', 'Lviv', 'SomeUser3', '+213133213', '2017-05-18 17:11:59'],
+            [4, 'Company4', 'Lviv', 'SomeUser5', '+342233423', '2017-05-19 17:11:59'],
+        ]);
+
+        $this->batchInsert('tender', ['id', 'name', 'description', 'price', 'begin_time', 'end_time', 'company_id'], [
+            [1, 'Tender1', 'Tender 1 for some companies', '100', '2017-05-16 17:16:59', '2017-05-20 18:16:59', '1'],
+            [2, 'Tender2', 'Tender 2 for some companies', '50', '2017-05-22 17:16:59', '2017-05-27 18:16:59', '2'],
+        ]);
+
+        $this->batchInsert('bid', ['id', 'description', 'price', 'begin_time', 'end_time', 'company_id', 'tender_id'], [
+            [1, 'Bid 1 for some tenders - company 3', '120', '2017-05-16 17:16:59', '2017-05-22 18:16:59', '3', '1'],
+            [2, 'Bid 2 for some tenders - company 4', '70', '2017-05-19 17:16:59', '2017-05-24 18:16:59', '4', '1'],
+            [3, 'Bid 3 for some tenders - company 3', '80', '2017-05-18 17:16:59', '2017-05-23 18:16:59', '3', '2'],
+            [4, 'Bid 4 for some tenders - company 4', '90', '2017-05-19 19:16:59', '2017-05-22 22:16:59', '4', '2'],
+        ]);
     }
 
     public function down()
